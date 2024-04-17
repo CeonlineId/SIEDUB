018eaf68921921b7a9a422d46dccd67b18c6dd82
FahmiMaulana5 <fahmifadila1933@gmail.com>
2024-04-17 19:06:27 +0700

    [General] All change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,26 +4,30 @@ import Img from '../assets/images/org.png';
 
 export default function Hero() {
   return (
-      <div>
-      <Navbar/>
-      <div className="grid grid-cols-2">
-        <div className="col-span-1 flex flex-col justify-center">
-          <h1 className="text-start font-bold text-4xl px-20 py-8">Selamat Datang Di <br/>SIEDUB</h1>
-          <p className="font-normal text-lg px-20 pb-10">Sistem Edukasi Bencana (SIEDUB) merupakan platform informasi dan edukasi kepada masyarakat luas dan aplikasi untuk proses pengaduan bencana yang terjadi disekitar masyarakat</p>
-          <div className="grid grid-cols-2 px-10">
-            <div className="col-span-1 flex flex-col justify-center">
-              <button className="bg-orange-600 border-2 hover:bg-orange-700 focus:bg-orange-700 text-white hover:text-white focus:text-white font-semibold py-2 px-20 rounded mx-auto text-start">
-                  Mulai
-              </button>
-            </div>
-            <div className="col-span-1 flex flex-col justify-center">
-              <a href="">Pelajari Lebih Lanjut</a>
-            </div>
+    <div className="grid grid-cols-2">
+      <div className="col-span-1 flex flex-col justify-center">
+        <h1 className="text-start font-bold text-4xl px-20 py-8">
+          Selamat Datang Di <br />
+          SIEDUB
+        </h1>
+        <p className="font-normal text-lg px-20 pb-10">
+          Sistem Edukasi Bencana (SIEDUB) merupakan platform informasi dan
+          edukasi kepada masyarakat luas dan aplikasi untuk proses pengaduan
+          bencana yang terjadi disekitar masyarakat
+        </p>
+        <div className="grid grid-cols-2 px-10">
+          <div className="col-span-1 flex flex-col justify-center">
+            <button className="bg-orange-600 border-2 hover:bg-orange-700 focus:bg-orange-700 text-white hover:text-white focus:text-white font-semibold py-2 px-20 rounded mx-auto text-start">
+              Mulai
+            </button>
+          </div>
+          <div className="col-span-1 flex flex-col justify-center">
+            <a href="">Pelajari Lebih Lanjut</a>
           </div>
         </div>
-        <div className="col-span-1 flex justify-center items-center">
-          <img src={Img} alt="Org" className="w-96 h-96 my-32 mx-40"/>
-        </div>
+      </div>
+      <div className="col-span-1 flex justify-center items-center">
+        <img src={Img} alt="Org" className="w-96 h-96 my-32 mx-40" />
       </div>
     </div>
   );
--- src/components/NotFoundSection.jsx
+++ src/components/NotFoundSection.jsx
@@ -1,11 +1,13 @@
 export default function NotFoundSection() {
   return (
-    <section className="flex items-center h-screen p-16 bg-gray-200q">
+    <section className="flex items-center h-screen p-16 bg-orange-600">
       <div className="container flex flex-col items-center ">
-        <div className="flex flex-col gap-6 max-w-md text-center">
-          <p className="text-2xl md:text-3xl font-semibold">404 Error</p>
-          <p className="text-2xl md:text-3xl">
-            Sorry, we couldn't find this page.
+        <div className="flex flex-col gap-6 text-center">
+          <p className="text-2xl md:text-5xl font-semibold text-white">
+            404 Error
+          </p>
+          <p className="text-sm md:text-4xl text-white">
+            Maaf, Halaman tidak ditemukan.
           </p>
         </div>
       </div>
