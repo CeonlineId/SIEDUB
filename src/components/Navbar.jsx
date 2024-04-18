bb2c95ed404b9d769950373df67077b4c9a01692
NAUFAL IRFAN HAIDAR <naufalirfanhaidar4@gmail.com>
2024-04-18 12:17:10 +0700

    Slicing [Navbar Responsive]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,145 +11,71 @@ const Navbar = () => {
   return (
     <nav className="bg-white text-black p-4 px-4 md:px-20 fixed top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-8 text-lg font-semibold">
-          <img src={logo} alt="Logo" className="w-25 h-14" />
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? 'text-black border-b-2 border-black'
-                : 'text-black hover:border-b-2 hover:border-black'
-            }
-          >
-            Beranda
-          </NavLink>
-          <div className="relative">
-            <a
-              href="#"
-              className="text-black"
-              onClick={() => setDropdownOpen(!dropdownOpen)}
+        <div className="flex items-center gap-4 text-lg font-semibold">
+          <img src={logo} alt="Logo" className="w-32 h-10" />
+          <div className="hidden md:flex items-center gap-8">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? 'text-black border-b-2 border-black'
+                  : 'text-black hover:border-b-2 hover:border-black'
+              }
             >
-              Edukasi
-            </a>
-            {dropdownOpen && (
-              <div className="absolute top-full left-0 bg-gray-800 shadow-md rounded-md mt-1 py-2 w-48 font-normal text-sm">
-                <NavLink
-                  to="/edukasi/banjir"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Banjir
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Kebakaran Hutan
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Longsor
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Tsunami
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Erupsi
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Gempa
-                </NavLink>
-              </div>
-            )}
+              Beranda
+            </NavLink>
+            <div className="relative">
+              <p
+                
+                className="text-black hover:cursor-pointer"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                Edukasi
+              </p>
+              {dropdownOpen && (
+                <div className="absolute top-full left-0 bg-gray-800 shadow-md rounded-md mt-1 py-2 w-48 font-normal text-sm">
+                  <NavLink
+                    to="/edukasi"
+                    className="block px-4 py-2 text-white hover:bg-gray-700"
+                  >
+                    Banjir
+                  </NavLink>
+                  <NavLink
+                    to="#"
+                    className="block px-4 py-2 text-white hover:bg-gray-700"
+                  >
+                    Kebakaran Hutan
+                  </NavLink>
+                  <NavLink
+                    to="#"
+                    className="block px-4 py-2 text-white hover:bg-gray-700"
+                  >
+                    Longsor
+                  </NavLink>
+                  <NavLink
+                    to="#"
+                    className="block px-4 py-2 text-white hover:bg-gray-700"
+                  >
+                    Tsunami
+                  </NavLink>
+                  <NavLink
+                    to="#"
+                    className="block px-4 py-2 text-white hover:bg-gray-700"
+                  >
+                    Erupsi
+                  </NavLink>
+                  <NavLink
+                    to="#"
+                    className="block px-4 py-2 text-white hover:bg-gray-700"
+                  >
+                    Gempa
+                  </NavLink>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <div className="flex gap-4 px-14">
-          <NavLink
-            to="/informasi"
-            className="border border-[#FF3D00] border-3 hover:bg-[#FF3D00] focus:bg-[#FF3D00] text-black hover:text-white focus:text-white font-semibold py-1 px-5 rounded"
-          >
-            Informasi
-          </NavLink>
-        </div>
-      </div>
-    </nav>
-  );
-  return (
-    <nav className="bg-white text-black p-4 px-20 fixed top-0 left-0 w-full z-10">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-8 text-lg font-semibold">
-          <img src={logo} alt="Logo" className="w-25 h-14" />
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? 'text-black border-b-2 border-black'
-                : 'text-black hover:border-b-2 hover:border-black'
-            }
-          >
-            Beranda
-          </NavLink>
-          <div className="relative">
-            <a
-              href="#"
-              className="text-black"
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-            >
-              Edukasi
-            </a>
-            {dropdownOpen && (
-              <div className="absolute top-full left-0 bg-gray-800 shadow-md rounded-md mt-1 py-2 w-48 font-normal text-sm">
-                <NavLink
-                  to="/edukasi"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Banjir
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Kebakaran Hutan
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Longsor
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Tsunami
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Erupsi
-                </NavLink>
-                <NavLink
-                  to="#"
-                  className="block px-4 py-2 text-white hover:bg-gray-700"
-                >
-                  Gempa
-                </NavLink>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="flex gap-4 px-14">
+        <div className="flex gap-4 px-4 md:px-14">
           <NavLink
             to="/informasi"
             className="border border-[#FF3D00] border-3 hover:bg-[#FF3D00] focus:bg-[#FF3D00] text-black hover:text-white focus:text-white font-semibold py-1 px-5 rounded hidden md:flex"
