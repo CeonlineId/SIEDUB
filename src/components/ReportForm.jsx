8e4b0054d908c64fdd338dcac2f0eed900ecce79
FahmiMaulana5 <fahmifadila1933@gmail.com>
2024-04-21 23:22:58 +0700

    [BE] Clean up add report data

--- src/components/ReportForm.jsx
+++ src/components/ReportForm.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/images/logoW.png';
 import tutor from '../assets/images/tutor.png';
-import { Link } from 'react-router-dom';
 import '../utils/firebaseConfig';
 import { getFirestore, addDoc, collection } from 'firebase/firestore';
 import { uploadImageToStorage } from '../utils/uploadImageToStorage';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const ReportForm = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -13,28 +13,43 @@ const ReportForm = () => {
   const [disasterType, setDisasterType] = useState('');
   const [location, setLocation] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
+  const [isSending, setIsSending] = useState(false);
 
   const db = getFirestore();
+  let navigate = useNavigate();
+
   const saveReportData = async () => {
-    const docRef = await addDoc(collection(db, 'report'), {
-      phoneNumber: phoneNumber,
-      fullName: fullName,
-      disasterType: disasterType,
-      location: location,
-      imageUrl: imageUrl,
-    });
-    alert('Laporan berhasil dikirim');
+    setIsSending(true);
+    try {
+      const docRef = await addDoc(collection(db, 'report'), {
+        phoneNumber: phoneNumber,
+        fullName: fullName,
+        disasterType: disasterType,
+        location: location,
+        imageUrl: imageUrl,
+      });
+      alert('Laporan berhasil dikirim!');
+      navigate('/');
+    } catch (error) {
+      console.error('Error saving data:', error);
+      alert('Gagal mengirim laporan!');
+    }
+    setIsSending(false);
   };
 
   const handleFileChange = async event => {
     const file = event.target.files[0];
     if (file) {
+      setIsUploading(true);
       try {
         const storagePath = `images/${file.name}`;
         const downloadURL = await uploadImageToStorage(file, storagePath);
         setImageUrl(downloadURL);
+        setIsUploading(false);
       } catch (error) {
-        console.error('Error mengunggah file:', error);
+        alert('Gagal menambahkan gambar!');
+        setIsUploading(false);
       }
     }
   };
@@ -43,6 +58,11 @@ const ReportForm = () => {
 
   return (
     <>
+      {(isUploading || isSending) && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+          <CircularProgress color="inherit" />
+        </div>
+      )}
       <nav className="bg-[#FF3D00] text-white p-4 px-20 fixed top-0 left-0 w-full z-10">
         <div className="container mx-auto flex justify-between items-center">
           <div className="flex items-center gap-4 text-lg font-semibold">
@@ -65,8 +85,19 @@ const ReportForm = () => {
                 onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 Edukasi
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                  />
                 </svg>
               </p>
               {dropdownOpen && (
@@ -132,8 +163,13 @@ const ReportForm = () => {
           d="M0,224L26.7,208C53.3,192,107,160,160,149.3C213.3,139,267,149,320,170.7C373.3,192,427,224,480,245.3C533.3,267,587,277,640,256C693.3,235,747,181,800,144C853.3,107,907,85,960,101.3C1013.3,117,1067,171,1120,181.3C1173.3,192,1227,160,1280,160C1333.3,160,1387,192,1413,208L1440,224L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"
         ></path>
       </svg>
-      <h1 className="text-4xl text-white text-center font-semibold mt-16 mb-4 relative z-1">Form Pelaporan Bencana</h1>
-      <p className="text-white relative z-2 text-center text-sm">Silahkan Laporkan kejadian <br />Bencana Alam Yang Terjadi di Daerah anda</p>
+      <h1 className="text-4xl text-white text-center font-semibold mt-16 mb-4 relative z-1">
+        Form Pelaporan Bencana
+      </h1>
+      <p className="text-white relative z-2 text-center text-sm">
+        Silahkan Laporkan kejadian <br />
+        Bencana Alam Yang Terjadi di Daerah anda
+      </p>
       <section className="mx-auto p-4 flex justify-center items-center min-h-screen relative">
         <form
           className="w-full max-w-full grid grid-cols-1 gap-y-2 bg-white p-10 m-32 mt-10 mb-10 text-center rounded-lg shadow-lg"
@@ -157,6 +193,7 @@ const ReportForm = () => {
             placeholder="Nomor Hp"
             value={phoneNumber}
             onChange={e => setPhoneNumber(e.target.value)}
+            required
           />
           <input
             type="text"
@@ -164,6 +201,7 @@ const ReportForm = () => {
             placeholder="Nama Lengkap"
             value={fullName}
             onChange={e => setFullName(e.target.value)}
+            required
           />
           <input
             type="text"
@@ -171,6 +209,7 @@ const ReportForm = () => {
             placeholder="Jenis Bencana"
             value={disasterType}
             onChange={e => setDisasterType(e.target.value)}
+            required
           />
           <input
             type="text"
@@ -178,6 +217,7 @@ const ReportForm = () => {
             placeholder="Lokasi"
             value={location}
             onChange={e => setLocation(e.target.value)}
+            required
           />
 
           <button className="bg-[#FF3D00] text-white p-2 rounded-xl w-19 h-19 block mx-auto relative">
@@ -187,6 +227,7 @@ const ReportForm = () => {
               type="file"
               className="absolute inset-0 opacity-0"
               onChange={handleFileChange}
+              required
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -209,7 +250,9 @@ const ReportForm = () => {
             </svg>
           </button>
 
-          <p className="text-sm text-slate-400">Kirim Foto Kejadian</p>
+          <p className="text-sm text-slate-400">
+            {imageUrl ? 'Gambar Tersimpan' : 'Kirim Foto Kejadian'}
+          </p>
           <button
             type="submit"
             className="w-24 h-10 justify-self-end bg-[#FF3D00] rounded-lg text-white"
